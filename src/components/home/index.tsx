a13fc4826635161884253a0ad5ddaabe37b6d0b0
Susanta0 <susanta721467@gmail.com>
2024-12-22 20:21:03 +0530

    finded some error and re-added code

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 import axios from "axios";
-import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
